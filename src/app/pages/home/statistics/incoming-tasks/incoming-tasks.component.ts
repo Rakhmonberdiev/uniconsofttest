944e0d61063a23a7d0d6bb168f72bc7241aaa5ef
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-07-06 18:46:46 +0500

    feat: добавить компоненты для входящих и исходящих задач с базовой разметкой

---
+++ src/app/pages/home/statistics/incoming-tasks/incoming-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incoming-tasks',
+  imports: [],
+  templateUrl: './incoming-tasks.component.html',
+  styleUrl: './incoming-tasks.component.scss'
+})
+export class IncomingTasksComponent {
+
+}
---
+++ src/app/pages/home/statistics/outgoing-tasks/outgoing-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outgoing-tasks',
+  imports: [],
+  templateUrl: './outgoing-tasks.component.html',
+  styleUrl: './outgoing-tasks.component.scss'
+})
+export class OutgoingTasksComponent {
+
+}
