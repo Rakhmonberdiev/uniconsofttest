1513f36bdf17c9db0b5da4544030deb55062b941
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-07-08 14:08:11 +0500

    виджет погоды

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { CalendarComponent } from '../../shared/calendar/calendar.component';
+import { WeatherComponent } from '../../shared/weather/weather.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NgClass, StatisticsComponent, CalendarComponent],
+  imports: [NgClass, StatisticsComponent, CalendarComponent, WeatherComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
